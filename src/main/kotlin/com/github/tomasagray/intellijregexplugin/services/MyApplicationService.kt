00fff7248600d230ff6512ec4bf9191cd210e716
GitHub Action <action@github.com>
2021-12-21 02:03:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tomasagray/intellijregexplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tomasagray.intellijregexplugin.services
+
+import com.github.tomasagray.intellijregexplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/tomasagray/intellijregexplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tomasagray/intellijregexplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tomasagray.intellijregexplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tomasagray.intellijregexplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
